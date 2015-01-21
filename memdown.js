065797f1667365e425edaaccbd302a74e7e8297c
Riceball LEE <snowyu.lee@gmail.com>
2015-01-21 22:42:32 +0800

    + final method to free global mem * clean code

--- memdown.js
+++ memdown.js
@@ -119,35 +119,6 @@ MemIterator.prototype._nextSync = function (callback) {
   return [key, value]
 }
 
-MemIterator.prototype._next = function (callback) {
-  var key
-    , value
-
-  if (this._done++ >= this._limit)
-    return setImmediate(callback)
-
-  if (!this._tree.valid)
-    return setImmediate(callback)
-
-  key = this._tree.key
-  value = this._tree.value
-
-  if (!this._test(key))
-    return setImmediate(callback)
-
-  if (this.keyAsBuffer)
-    key = new Buffer(key)
-
-  if (this.valueAsBuffer)
-    value = new Buffer(value)
-
-  this._tree[this._incr]()
-
-  setImmediate(function callNext() {
-    callback(null, key, value)
-  })
-}
-
 MemIterator.prototype._test = function () {return true}
 
 function MemDOWN (location) {
@@ -163,22 +134,17 @@ function MemDOWN (location) {
 
 inherits(MemDOWN, AbstractLevelDOWN)
 
+MemDOWN.prototype['final'] = function() {
+  if (this._store !== this) delete globalStore[this._location]
+}
+
 MemDOWN.prototype._openSync = function() {
   return true
-}
-MemDOWN.prototype._open = function (options, callback) {
-  var self = this
-  setImmediate(function callNext() { callback(null, self) })
 }
 
 MemDOWN.prototype._putSync = function (key, value, options, callback) {
   this._store[this._location] = this._store[this._location].remove(key).insert(key, value)
   return true
-}
-
-MemDOWN.prototype._put = function (key, value, options, callback) {
-  this._store[this._location] = this._store[this._location].remove(key).insert(key, value)
-  setImmediate(callback)
 }
 
 MemDOWN.prototype._isExistsSync = function (key, options) {
@@ -208,32 +174,9 @@ MemDOWN.prototype._getSync = function (key, options) {
   return value
 }
 
-MemDOWN.prototype._get = function (key, options, callback) {
-  var value = this._store[this._location].get(key)
-
-  if (value === undefined) {
-    // 'NotFound' error, consistent with LevelDOWN API
-    var err = new NotFoundError()
-    return setImmediate(function callNext() { callback(err) })
-  }
-
-  if (options.asBuffer === true && !this._isBuffer(value))
-    value = new Buffer(String(value))
-  
-  setImmediate(function callNext () {
-    callback(null, value)
-  })
-
-}
-
 MemDOWN.prototype._delSync = function (key, options) {
   this._store[this._location] = this._store[this._location].remove(key)
   return true
-}
-
-MemDOWN.prototype._del = function (key, options, callback) {
-  this._store[this._location] = this._store[this._location].remove(key)
-  setImmediate(callback)
 }
 
 MemDOWN.prototype._batchSync = function (array, options) {
@@ -274,51 +217,7 @@ MemDOWN.prototype._batchSync = function (array, options) {
   return true
 }
 
-MemDOWN.prototype._batch = function (array, options, callback) {
-  var err
-    , i = -1
-    , key
-    , value
-    , len = array.length
-    , tree = this._store[this._location]
-
-  while (++i < len) {
-    if (!array[i])
-      continue;
-    
-    key = this._isBuffer(array[i].key) ? array[i].key : String(array[i].key)
-    err = this._checkKey(key, 'key')
-    if (err)
-      return setImmediate(function errorCall() { callback(err) })
-    
-    tree = tree.remove(array[i].key)
-    // we always remove as insert doesn't replace
-
-    if (array[i].type === 'put') {
-
-      value = this._isBuffer(array[i].value) ? array[i].value : String(array[i].value)
-      err = this._checkKey(value, 'value')
-
-      if (err)
-        return setImmediate(function errorCall() { callback(err) })
-
-      tree = tree.insert(key, value)
-    }
-  
-  }
-  
-  this._store[this._location] = tree;
-
-  setImmediate(callback)
-}
-
 MemDOWN.prototype.IteratorClass = MemIterator
-
-/*
-MemDOWN.prototype._iterator = function (options) {
-  return new MemIterator(this, options)
-}
-*/
 
 MemDOWN.prototype._isBuffer = function (obj) {
   return Buffer.isBuffer(obj)
